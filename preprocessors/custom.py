9a88a35fe5bc2f2111ea6fcd7f97261ea9633716
imlixinyang <1034298388@qq.com>
2022-10-19 22:51:40 +0800

    Fix custom.py

--- preprocessors/custom.py
+++ preprocessors/custom.py
@@ -21,6 +21,6 @@ for tag_dir in tag_dirs:
         open(os.path.join(target_path, f'{tag_dir}_{attribute_dir}.txt'), 'w')
         images = os.listdir(os.path.join(opts.img_path, tag_dir, attribute_dir))
         for image in images:
-            if os.path.isfile(image):
+            if os.path.isfile(os.path.join(opts.imgs, tag_dir, attribute_dir, image)):
                 with open(os.path.join(target_path, f'{tag_dir}_{attribute_dir}.txt'), mode='a') as f:
                     f.write(f'{os.path.abspath(os.path.join(opts.imgs, tag_dir, attribute_dir, image))} 0\n')
